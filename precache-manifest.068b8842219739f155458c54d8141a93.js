df450a0131d522a99fabc5b5cebe66783abe931a
YuraBobeshko <gorobech55@gmail.com>
2019-11-06 10:14:46 +0200

    Updates

---
+++ precache-manifest.068b8842219739f155458c54d8141a93.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3df949275e60af310dedc0b62f1459",
+    "url": "/react_dynamic-list-of-todos/index.html"
+  },
+  {
+    "revision": "173fb7d7dddb50094ecf",
+    "url": "/react_dynamic-list-of-todos/static/css/main.ec9505b9.chunk.css"
+  },
+  {
+    "revision": "b76fb8522e14f5eabbea",
+    "url": "/react_dynamic-list-of-todos/static/js/2.03ea7c7e.chunk.js"
+  },
+  {
+    "revision": "173fb7d7dddb50094ecf",
+    "url": "/react_dynamic-list-of-todos/static/js/main.750d5b36.chunk.js"
+  },
+  {
+    "revision": "04977e40ef1cd22bcb98",
+    "url": "/react_dynamic-list-of-todos/static/js/runtime~main.b6f1ef06.js"
+  }
+]);
